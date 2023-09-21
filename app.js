82227abd2d74956336785ae956838d608ea0bea3
jeeeanlefchak <jeeeanlefchak>
2023-09-21 07:43:25 -0300

    ss

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const { WebcastPushConnection } = require('tiktok-live-connector');
-let tiktokUsername = "jeeeanlefchak";
+let tiktokUsername = "banggjean";
 let tiktokLiveConnection = new WebcastPushConnection(tiktokUsername);
 const gTTS = require('gtts');
 var player = require('play-sound')(opts = {})
+setInterval(bemVindo, 50000);
+
+tiktokLiveConnection.on('disconnected', () => {
+    console.log('Disconnected :(');
+})
+
+tiktokLiveConnection.on('streamEnd', (actionId) => {
+    if (actionId === 3) {
+        console.log('Stream ended by user');
+    }
+    if (actionId === 4) {
+        console.log('Stream ended by platform moderator (ban)');
+    }
+})
 
 tiktokLiveConnection.on('chat', data => {
     console.log('\x1b[34m', `${data.uniqueId.toUpperCase()}: ${data.comment}`);
@@ -12,25 +26,38 @@ tiktokLiveConnection.connect().then(state => {
     // console.info(`Connected to roomId ${state.roomId}`);
 }).catch(err => {
     console.error('Failed to connect', err);
-    
+
 })
+
+function reproduzirAudio(speech) {
+    let aleatoryName = `Audio/${(Math.random() + 1).toString(36).substring(7)}.mp3`;
+    const gtts = new gTTS(speech, 'pt-BR');
+    gtts.save(`${aleatoryName}`, function (err, result) {
+        if (!err) {
+            player.play(aleatoryName);
+        }
+    });
+}
 
 tiktokLiveConnection.getAvailableGifts().then(giftList => {
     this.giftList = giftList;
     tiktokLiveConnection.on('gift', data => {
-        let item = giftList.find(x => x.id == data.giftId);
-        console.log('\x1b[31m',`${data.uniqueId.toUpperCase()} mandou um PRESENTE ${item.name}`);
+        //let item = giftList.find(x => x.id == data.giftId);
+        console.log('\x1b[31m', `${data.uniqueId.toUpperCase()} mandou um PRESENTE ${data.giftName} x${data.repeatCount}`);
 
-        let speech = `${data.nickname} ${item.name} Obrigado pelo presente!`;
-        const gtts = new gTTS(speech, 'pt-BR');
-        let aleatoryName = `Audio/${(Math.random() + 1).toString(36).substring(7)}.mp3`;
-        gtts.save(`${aleatoryName}`, function (err, result) {
-            if (!err) {
-                player.play(aleatoryName);
-            }
-        });
-
+        let speech = `${data.nickname} Obrigado pelo presente ${data.giftName}`;
+        reproduzirAudio(speech);
     })
 }).catch(err => {
     console.error(err);
 })
+
+tiktokLiveConnection.on('share', (data) => {
+    console.log(data.uniqueId, "shared the stream!");
+    let speech = `${data.nickname} Compartilhou a live`;
+    reproduzirAudio(speech);
+})
+
+function bemVindo() {
+    reproduzirAudio("Bem vindo a Live");
+}
